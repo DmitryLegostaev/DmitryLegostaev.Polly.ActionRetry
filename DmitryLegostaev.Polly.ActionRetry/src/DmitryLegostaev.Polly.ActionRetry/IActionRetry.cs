678d013165471c933e4d06174b0b94e59d2ca7c3
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-07-09 00:37:49 +0300

    Added IActionRetry

---
+++ DmitryLegostaev.Polly.ActionRetry/src/DmitryLegostaev.Polly.ActionRetry/IActionRetry.cs
@@ -0,0 +1,23 @@
+﻿using DmitryLegostaev.Polly.ConditionalWait.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DmitryLegostaev.Polly.ActionRetry;
+
+public interface IActionRetry
+{
+    public void DoWithRetry(Action action,
+        int retryCount, TimeSpan backOffDelay,
+        IList<Type>? exceptionsToIgnore = null, string? failReason = null, string? codePurpose = null, ILogger? logger = null);
+
+    public void DoWithRetry(Action action,
+        IWaitConfiguration waitConfiguration,
+        IList<Type>? exceptionsToIgnore = null, string? failReason = null, string? codePurpose = null, ILogger? logger = null);
+
+    public T DoWithRetry<T>(Func<T> function,
+        int retryCount, TimeSpan backOffDelay,
+        IList<Type>? exceptionsToIgnore = null, string? failReason = null, string? codePurpose = null, ILogger? logger = null);
+
+    public T DoWithRetry<T>(Func<T> function,
+        IWaitConfiguration waitConfiguration,
+        IList<Type>? exceptionsToIgnore = null, string? failReason = null, string? codePurpose = null, ILogger? logger = null);
+}
